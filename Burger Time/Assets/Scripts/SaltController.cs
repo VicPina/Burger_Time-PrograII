dafd5eb280150717c7792ea140c69abdd325646b
VicPina <vicpanzu7@gmail.com>
2020-03-03 19:24:26 -0600

    Update SaltController.cs

--- Burger Time/Assets/Scripts/SaltController.cs
+++ Burger Time/Assets/Scripts/SaltController.cs
@@ -13,6 +13,15 @@ public class SaltController : MonoBehaviour
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        
+    /*
+         //Get the new position of our bullet
+        var y = transform.position.y + Time.deltaTime * speed;
+
+        //Set the position of our bullet through the RigidBody2D component (since we are using physics)
+        rigidBody.MovePosition(new Vector2(transform.position.x, y));
+    */
+
         //Destroy the bullet if it didn't hit anything after 10 seconds
         Destroy(gameObject, timeBeforeDestruction);
     }
